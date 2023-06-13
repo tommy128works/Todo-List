11de83ce1a18b68161181b9b7a02150c8466c83e
Tommy <tommy128works@gmail.com>
2023-06-13 08:48:43 -0400

    feat: add lo-fi proto of navigation buttons
    Created low fidelity prototype of navigation buttons.

--- src/js/index.js
+++ src/js/index.js
@@ -4,18 +4,12 @@ import * as bootstrap from "bootstrap";
 // modules
 // module for todos that contains all properties
 
-
 // user interface module > imports other modules to divide UI into components
 import header from "./header";
+import navigationButtons from "./navigationButtons";
 
 document.body.appendChild(header());
-
-// buttons: All Tasks, Today, Next 7 Days, Important
-// buttons container needs to respond and wrap for mobile view
-
-let div = document.createElement("div");
-
-
+document.body.appendChild(navigationButtons());
 
 // create eventHandlers to load different filters
 // create module for each page

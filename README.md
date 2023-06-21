e447cc8f1169766b2616b91bfac793373a69e17e
Tommy <tommy128works@gmail.com>
2023-06-21 13:13:18 -0400

    feat: style footer and add github link
    Styled footer and added github link.

--- README.md
+++ README.md
@@ -8,11 +8,9 @@ I have learned that Webpacks have many uses for development and production. A fe
 was webpack-dev-server which automatically refreshed the page.
 I have also found ES6 Modules to be a useful tool in reusing code between files and projects.
 
-Photo Credits:
-Background image: https://www.pexels.com/photo/green-pine-trees-1179229/
-Cherry blossom image: https://www.pexels.com/photo/blooming-sakura-in-yard-of-japanese-house-4087904/
-Crabapple image: https://www.pexels.com/photo/crabapple-flowers-in-bloom-11808096/
-Maple image: https://www.pexels.com/photo/person-holding-red-maple-leaf-1144686/
+Credits:
+
+Github icon: https://www.flaticon.com/free-icons/github"
 
 This is a guided project by The Odin Project (https://www.theodinproject.com/)
-Reference link: https://www.theodinproject.com/lessons/node-path-javascript-restaurant-page
+Reference link: ///
--- src/js/index.js
+++ src/js/index.js
@@ -12,21 +12,23 @@ import { header, addToggleSidebarEventHandler } from "./header";
 import navigationToDos from "./navigationToDos";
 import navigationProjects from "./navigationProjects";
 import contentSection from "./contentSection";
+import footer from "./footer";
 
 document.body.appendChild(header());
 let mainContainer = document.createElement("div");
+mainContainer.classList.add("main-container");
 let sideBarContainer = document.createElement("div");
 sideBarContainer.classList.add("sidebar-section");
 sideBarContainer.setAttribute("id", "sidebar");
 sideBarContainer.appendChild(navigationToDos());
 sideBarContainer.appendChild(navigationProjects());
 
-mainContainer.classList.add("main-container");
 mainContainer.appendChild(sideBarContainer);
 mainContainer.appendChild(contentSection());
 document.body.appendChild(mainContainer);
-
 addToggleSidebarEventHandler();
+
+document.body.appendChild(footer());
 
 // create eventHandlers to load different filters
 // create module for each page

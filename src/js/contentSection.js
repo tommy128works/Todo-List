2951afc50cda2368d639eb911d36ee7e05d2e08c
Tommy <tommy128works@gmail.com>
2023-06-20 20:41:21 -0400

    feat: style content section with dummy content
    The content section was styled using dummy content. A button was added
    to allow the user to add a new task. No functionality has been added.

--- src/js/contentSection.js
+++ src/js/contentSection.js
@@ -1,41 +1,80 @@
-// make dummy Title
-// make one dummy Task
-const contentSection = () => {
-  let container = document.createElement("div");
-  container.classList.add("large-font", "content-section");
-  container.textContent = "All Tasks";
+// make dummy Title amd dummy task for now
 
-  let btn = document.createElement("btn");
-  btn.classList.add("btn", "btn-secondary", "btn-primary", "project");
-  btn.classList.toggle("btn-primary");
-  btn.setAttribute("type", "button");
+const createTitle = () => {
+  let div = document.createElement("div");
+  div.classList.add("large-font");
+  div.textContent = "All Tasks"; // This title changes based on page
+
+  return div;
+}
+
+const createTaskItem = (title, description, date, priority) => {
+  let container = document.createElement("div");
+  container.classList.add("task-item");
+
   let span = document.createElement("span");
   span.classList.add("material-symbols-outlined");
-  span.textContent = "drag_indicator";
-  btn.appendChild(span);
-  span = document.createElement("span");
-  span.classList.add("project-title");
-  span.textContent = "Project ONE";
-  btn.appendChild(span);
+  span.textContent = "radio_button_unchecked";
+  container.appendChild(span);
+
+  let textContainer = document.createElement("div");
+  textContainer.classList.add("task-item-text");
+  
+  let taskTitle = document.createElement("div");
+  taskTitle.classList.add("small-font");
+  taskTitle.textContent = title;
+  textContainer.appendChild(taskTitle);
+  let taskDescription = document.createElement("div");
+  taskDescription.classList.add("smallest-font");
+  taskDescription.textContent = description;
+  textContainer.appendChild(taskDescription);
+  container.appendChild(textContainer);
+
+  let div = document.createElement("div");
+  div.classList.add("small-font");
+  div.textContent = date;
+  container.appendChild(div);
+
+  // I plan to add priority inside fancy icon and add margin
+  div = document.createElement("div");
+  div.classList.add("small-font");
+  div.textContent = priority;
+  container.appendChild(div);
+
   span = document.createElement("span");
   span.classList.add("material-symbols-outlined");
   span.textContent = "more_vert";
-  btn.appendChild(span);
-  container.appendChild(btn);
+  container.appendChild(span);
 
-  btn = document.createElement("btn");
-  btn.classList.add("btn", "btn-secondary", "btn-primary", "project");
-  btn.classList.toggle("btn-primary");
+  return container;
+};
+
+const createAddTaskButton = () => {
+  let btn = document.createElement("btn");
+  btn.classList.add("btn", "btn-secondary", "add-new-task-btn");
   btn.setAttribute("type", "button");
-  span = document.createElement("span");
+  
+  let span = document.createElement("span");
   span.classList.add("material-symbols-outlined");
   span.textContent = "add_circle";
   btn.appendChild(span);
+  
   span = document.createElement("span");
   span.classList.add("project-title");
-  span.textContent = "Add Project";
+  span.textContent = "Add New Task";
   btn.appendChild(span);
-  container.appendChild(btn);
+  
+  return btn;
+}
+
+const contentSection = () => {
+  let container = document.createElement("div");
+  container.classList.add("content-section");
+  
+  container.appendChild(createTitle());
+  container.appendChild(createTaskItem("tempTitle", "tempDescription", "date format undecided", 5));
+  container.appendChild(createAddTaskButton());
+  
 
   return container;
 };

3851b91a62f9a109537d5a6d9ebb2a05185a1cf8
Tommy <tommy128works@gmail.com>
2023-06-23 13:43:28 -0400

    feat: add duplicate names check for project form
    User will be alerted if they enter an existing project name into the
    project form.

--- src/js/dataHandler.js
+++ src/js/dataHandler.js
@@ -4,6 +4,11 @@ import { updateStorage, displayStorage } from "./dataStorage";
 
 let projectsArray = [];
 
+const checkProjectsDuplicates = (projectsArray, string) => {
+  let projectTitles = projectsArray.map((project) => project.title);
+  return projectTitles.includes(string);
+}
+
 const addAddProjectFormEventListeners = () => {
   let addBtn = document.getElementById("add-project-button");
   let submitBtn = document.getElementById("submit-project-button");
@@ -14,15 +19,21 @@ const addAddProjectFormEventListeners = () => {
   addBtn.addEventListener("click", (event) => {
     form.style.display = "block";
     addBtn.style.display = "none";
+    projectName.focus();
   });
 
   submitBtn.addEventListener("click", (event) => {
     if (projectName.value === "") {
       alert("Project name cannot be empty!");
+    } else if (checkProjectsDuplicates(projectsArray, projectName.value)) {
+      alert("Project names must be different");
     } else {
-      // projectsArray.push(createProject(projectName.value));
-      // updateStorage(projectsArray);
-      // console.log(displayStorage());
+      projectsArray.push(createProject(projectName.value));
+      updateStorage(projectsArray);
+
+      // for testing only
+      console.log(displayStorage());
+      console.log(projectsArray);
 
       // needs to send input value to createProject() and projectsArray
       form.style.display = "none";
@@ -46,7 +57,7 @@ const addAddProjectFormEventListeners = () => {
         document.getElementById("cancel-project-button").click();
         break;
     }
-  })
+  });
 };
 
 const addAddTaskFormEventListeners = () => {
@@ -54,10 +65,12 @@ const addAddTaskFormEventListeners = () => {
   let submitBtn = document.getElementById("submit-task-button");
   let cancelBtn = document.getElementById("cancel-task-button");
   let form = document.getElementById("task-form");
+  let taskTitle = document.getElementById("task_title");
 
   addBtn.addEventListener("click", (event) => {
     form.style.display = "flex";
     addBtn.style.display = "none";
+    taskTitle.focus();
   });
 
   submitBtn.addEventListener("click", (event) => {
@@ -79,7 +92,7 @@ const addAddTaskFormEventListeners = () => {
         document.getElementById("cancel-task-button").click();
         break;
     }
-  })
+  });
 };
 
 export { addAddProjectFormEventListeners, addAddTaskFormEventListeners };
--- src/js/dataStorage.js
+++ src/js/dataStorage.js
@@ -54,15 +54,8 @@ const updateStorage = (projectsArray) => {
 // retrieve data from storage to display (part of refresh page function)
 // for now, display all projects
 const displayStorage = () => {
-  var values = [],
-    keys = Object.keys(localStorage),
-    i = keys.length;
-
-  while (i--) {
-    values.push(localStorage.getItem(keys[i]));
-  }
-
-  return values;
+  const items = { ...localStorage };
+  return items;
 };
 
 // delete everything function
